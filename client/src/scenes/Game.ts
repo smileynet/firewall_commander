40526f1cd3833decfa1b8c73f0fdeb73fdaffd11
Sam Biggins <sambiggins@gmail.com>
2023-06-22 12:38:48 -0700

    feat(entity): simple entity movement

--- client/src/scenes/Game.ts
+++ client/src/scenes/Game.ts
@@ -2,19 +2,22 @@ import Phaser from 'phaser';
 import {spawnEntity} from "@src/entity/spawnEntity";
 import {spawnMap} from "@src/map/spawnMap";
 import Vector2 = Phaser.Math.Vector2;
-import {SpawnPoint} from "@src/map/interaces";
+import {MapPath} from "@src/map/interaces";
 
 export default class GameScene extends Phaser.Scene {
-  spawnPoint: SpawnPoint;
+  private mapPath!: MapPath;
 
   constructor() {
     super('GameScene');
-    this.spawnPoint = {x: 100, y: 100};
+    this.mapPath = {
+      spawnPoint: new Vector2(100,100),
+      exitPoint: new Vector2(800,600)
+    };
   }
 
   create() {
     spawnMap(this);
-    spawnEntity(this, 'firewall', new Vector2(200, 100));
-    spawnEntity(this, 'malware', new Vector2(this.spawnPoint.x, this.spawnPoint.y));
+    spawnEntity(this, 'firewall', {spawnPoint: new Vector2(200, 100), exitPoint: new Vector2(200, 100)});
+    spawnEntity(this, 'malware', this.mapPath);
   }
 }
