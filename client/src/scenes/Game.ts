09935adbab7f46f4a2778518923f0250c4effa1d
Sam Biggins <sambiggins@gmail.com>
2023-06-22 12:59:36 -0700

    feat: enemy movement (#9) (#10)
    * v0.1.0
    
    * chore: synchronize package versions and names
    
    * fix: restore missing GameScene code
    
    * refactor: correct entity var name
    
    * feat(map): spawn entities at spawn point
    
    * Added firewall tower
    
    * Added firewall tower
    
    * Added all other assets
    
    * feat(entity): simple entity movement
    
    * refactor(entity): create entity feature folder, move related code
    
    * refactor: correct entity var name
    
    * feat(map): spawn entities at spawn point
    
    * feat(entity): simple entity movement
    
    ---------
    
    Co-authored-by: danielw-aws <daneiljw@amazon.com>

--- client/src/scenes/Game.ts
+++ client/src/scenes/Game.ts
@@ -1,29 +1,23 @@
 import Phaser from 'phaser';
+import {spawnEntity} from "@src/entity/spawnEntity";
+import {spawnMap} from "@src/map/spawnMap";
 import Vector2 = Phaser.Math.Vector2;
-
-function spawnMap(scene: Phaser.Scene) {
-  const map = scene.make.tilemap({key: 'map'});
-  const tileset = map.addTilesetImage('tiles');
-  const layer = map.createLayer('background', tileset, 0, 0);
-  console.debug("layer", layer);
-}
-
-function spawnEntity(scene: Phaser.Scene, type: string, coords: Vector2) {
-  const firewall = scene.add.image(coords.x, coords.y, type);
-  firewall.setDisplaySize(64, 64)
-  firewall.setInteractive().on('pointerdown', () => {
-    console.log(`${type} was clicked!`);
-  });
-}
+import {MapPath} from "@src/map/interaces";
 
 export default class GameScene extends Phaser.Scene {
+  private mapPath!: MapPath;
+
   constructor() {
     super('GameScene');
+    this.mapPath = {
+      spawnPoint: new Vector2(100,100),
+      exitPoint: new Vector2(800,600)
+    };
   }
 
   create() {
     spawnMap(this);
-    spawnEntity(this, 'firewall', new Vector2(200, 100));
-    spawnEntity(this, 'malware', new Vector2(300, 100));
+    spawnEntity(this, 'firewall', {spawnPoint: new Vector2(200, 100), exitPoint: new Vector2(200, 100)});
+    spawnEntity(this, 'malware', this.mapPath);
   }
 }
