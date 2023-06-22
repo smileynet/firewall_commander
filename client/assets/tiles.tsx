0d970c0956b2a723a11a7de7a462720db5a89c54
Sam Biggins <sambiggins@gmail.com>
2023-06-22 11:50:57 -0700

    feat: add map tile set

---
+++ client/assets/tiles.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tiles" tilewidth="256" tileheight="256" tilecount="45" columns="9">
+ <grid orientation="orthogonal" width="64" height="64"/>
+ <image source="tiles.png" width="2362" height="1354"/>
+</tileset>
