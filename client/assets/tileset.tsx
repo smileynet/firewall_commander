8d2cda5cd714889f6f7b095f9fcf3cc87c5ce0aa
Sam Biggins <sambiggins@gmail.com>
2023-06-22 11:56:10 -0700

    chore: unfuck things

--- client/assets/tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="tileset" tilewidth="64" tileheight="64" tilecount="756" columns="36" objectalignment="topright">
- <image source="tiles.png" width="2362" height="1354"/>
-</tileset>
--- client/src/scenes/Game.ts
+++ client/src/scenes/Game.ts
@@ -1,4 +1,20 @@
 import Phaser from 'phaser';
+import Vector2 = Phaser.Math.Vector2;
+
+function spawnMap(scene: Phaser.Scene) {
+  const map = scene.make.tilemap({key: 'map'});
+  const tileset = map.addTilesetImage('tiles');
+  const layer = map.createLayer('background', tileset, 0, 0);
+  console.debug("layer", layer);
+}
+
+function spawnEntity(scene: Phaser.Scene, type: string, coords: Vector2) {
+  const firewall = scene.add.image(coords.x, coords.y, type);
+  firewall.setDisplaySize(64, 64)
+  firewall.setInteractive().on('pointerdown', () => {
+    console.log(`${type} was clicked!`);
+  });
+}
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
@@ -6,13 +22,8 @@ export default class GameScene extends Phaser.Scene {
   }
 
   create() {
-    const map = this.make.tilemap({ key: 'map' });
-    const tileset = map.addTilesetImage('tiles');
-    const layer = map.createLayer('background', tileset, 0, 0);
-    console.debug("layer", layer);
-    const firewall = this.add.image(200, 70, 'firewall');
-    firewall.setDisplaySize(64, 64)
-    const malware = this.add.image(400, 70, 'malware');
-    malware.setDisplaySize(64, 64)
+    spawnMap(this);
+    spawnEntity(this, 'firewall', new Vector2(200, 100));
+    spawnEntity(this, 'malware', new Vector2(300, 100));
   }
 }
