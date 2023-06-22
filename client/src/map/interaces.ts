bb27210416af56c727b80ed448ea8f11a9126890
Sam Biggins <sambiggins@gmail.com>
2023-06-22 12:22:57 -0700

    feat(map): spawn entities at spawn point

---
+++ client/src/map/interaces.ts
@@ -0,0 +1,14 @@
+export interface SpawnPoint {
+    x: number;
+    y: number;
+}
+
+export interface EndPoint {
+    x: number;
+    y: number;
+}
+
+export interface MapPaths {
+    spawnPoint: SpawnPoint;
+    endPoint: EndPoint;
+}
