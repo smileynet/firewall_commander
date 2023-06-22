1b237b619b5a40560904aed4b0e7ef44e6b0610f
Sam Biggins <sambiggins@gmail.com>
2023-06-22 12:13:52 -0700

    refactor: correct entity var name

--- client/src/entity/spawnEntity.ts
+++ client/src/entity/spawnEntity.ts
@@ -2,9 +2,9 @@ import Phaser from "phaser";
 import Vector2 = Phaser.Math.Vector2;
 
 export function spawnEntity(scene: Phaser.Scene, type: string, coords: Vector2) {
-    const firewall = scene.add.image(coords.x, coords.y, type);
-    firewall.setDisplaySize(64, 64)
-    firewall.setInteractive().on('pointerdown', () => {
+    const entity = scene.add.image(coords.x, coords.y, type);
+    entity.setDisplaySize(64, 64)
+    entity.setInteractive().on('pointerdown', () => {
         console.log(`${type} was clicked!`);
     });
 }
