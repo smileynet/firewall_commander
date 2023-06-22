6970ae4f41162d81b67523809f7280259a2d156f
kimhansu <kimhansu@amazon.com>
2023-06-22 14:06:06 -0400

    Updated interfaces.ts

---
+++ electron/src/interfaces.ts
@@ -0,0 +1,82 @@
+// Feature interfaces
+
+export interface entity {
+    id: number;
+    name: string;
+}
+
+/*
+ * Interface for defensive combat feature
+ */
+export interface target {
+    health: number;
+    isAlive: boolean;
+    targetable: boolean;
+    invincible: boolean;
+}
+
+/*
+ * Interface for offensive combat feature
+ */
+export interface attacker {
+    atkDamage: number;
+    atkSpeed: number;
+    atkRange: number;
+    canAttack: boolean;
+}
+
+/*
+ * Interface for movement
+ */
+export interface mobile {
+    movementSpeed: number;
+}
+
+/*
+ * Interface for allegiance (true for ally or false for enemy)
+ */
+export interface allegiance {
+    isAlly: boolean;
+}
+
+/*
+ * Interface for upgrades
+ */
+export interface upgradable {
+    upgradeLevel: number;
+    canUpgrade: boolean;
+    upgradeCost: number;
+}
+
+/*
+ * Interface for placement
+ */
+export interface placeable {
+    isPlaced: boolean;
+    isStatic: boolean;
+    locX: number;
+    locY: number;
+}
+
+/*
+ * Interface for towers
+ */
+export interface tower extends entity, target, attacker, allegiance, upgradable, placeable {
+
+}
+
+/*
+ * Interface for units
+ */
+export interface unit extends entity, target, attacker, mobile, allegiance, placeable {
+
+}
+
+export interface projectile {
+    locX: number;
+    locY: number;
+    velX: number;
+    velY: number;
+    damage: number;
+    isActive: boolean;
+}
