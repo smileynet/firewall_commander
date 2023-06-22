f6cbdfdbca2c3571b0431be5a0454d0347819583
Sam Biggins <sambiggins@gmail.com>
2023-06-22 12:22:57 -0700

    feat(map): spawn entities at spawn point

--- client/src/scenes/Game.ts
+++ client/src/scenes/Game.ts
@@ -1,22 +1,20 @@
 import Phaser from 'phaser';
 import {spawnEntity} from "@src/entity/spawnEntity";
+import {spawnMap} from "@src/map/spawnMap";
 import Vector2 = Phaser.Math.Vector2;
-
-function spawnMap(scene: Phaser.Scene) {
-  const map = scene.make.tilemap({key: 'map'});
-  const tileset = map.addTilesetImage('tiles');
-  const layer = map.createLayer('background', tileset, 0, 0);
-  console.debug("layer", layer);
-}
+import {SpawnPoint} from "@src/map/interaces";
 
 export default class GameScene extends Phaser.Scene {
+  spawnPoint: SpawnPoint;
+
   constructor() {
     super('GameScene');
+    this.spawnPoint = {x: 100, y: 100};
   }
 
   create() {
     spawnMap(this);
     spawnEntity(this, 'firewall', new Vector2(200, 100));
-    spawnEntity(this, 'malware', new Vector2(300, 100));
+    spawnEntity(this, 'malware', new Vector2(this.spawnPoint.x, this.spawnPoint.y));
   }
 }
